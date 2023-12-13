0315eefe695762f603e381b929881f52d7753786
Jarosław Kątnik <jarek@katnik.pl>
2023-12-13 19:20:22 +0100

    Game record

---
+++ src/main/java/com/sportradar/scoreboard/Game.java
@@ -0,0 +1,17 @@
+package com.sportradar.scoreboard;
+
+public record Game(
+  String homeTeam,
+  String awayTeam,
+  int homeScore,
+  int awayScore,
+  long startTime) {
+
+  public Game(String homeTeam, String awayTeam) {
+    this(homeTeam, awayTeam, 0, 0, System.currentTimeMillis());
+  }
+
+  public Game updateScore(int updatedHomeScore, int updatedAwayScore) {
+    return new Game(homeTeam, awayTeam, updatedHomeScore, updatedAwayScore, startTime);
+  }
+}
---
+++ src/test/java/com/sportradar/scoreboard/GameTest.java
@@ -0,0 +1,41 @@
+package com.sportradar.scoreboard;
+
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+
+  public static final Offset<Long> SMALL_TIME_OFFSET = Offset.offset(100L);
+
+  @Test
+  void shouldCreateGame() {
+    // when
+    Game game = new Game("Home", "Away");
+
+    // then
+    assertThat(game.homeTeam()).isEqualTo("Home");
+    assertThat(game.awayTeam()).isEqualTo("Away");
+    assertThat(game.homeScore()).isEqualTo(0);
+    assertThat(game.awayScore()).isEqualTo(0);
+    assertThat(game.startTime()).isCloseTo(System.currentTimeMillis(), SMALL_TIME_OFFSET);
+  }
+
+  @CsvSource({
+    "1, 0",
+    "0, 1",
+  })
+  void shouldUpdateScore(int homeScore, int awayScore) {
+    // given
+    var game = new Game("Home", "Away");
+
+    // when
+    var updatedGame = game.updateScore(homeScore, awayScore);
+
+    // then
+    assertThat(updatedGame.homeScore()).isEqualTo(homeScore);
+    assertThat(updatedGame.awayScore()).isEqualTo(awayScore);
+  }
+}
