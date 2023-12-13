7d1accfc2282a6f792a5ca53991ddb20d967a507
Jarosław Kątnik <jarek@katnik.pl>
2023-12-13 19:58:36 +0100

    SortableScoreboardTest stub

---
+++ src/test/java/com/sportradar/scoreboard/SortableScoreboardTest.java
@@ -0,0 +1,25 @@
+package com.sportradar.scoreboard;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SortableScoreboardTest {
+  private Scoreboard scoreboard;
+
+  @BeforeEach
+  void setUp() {
+    scoreboard = ScoreboardFactory.createDefaultSortedScoreboard();
+  }
+
+  @Test
+  void testStartGame() {
+  }
+
+  @Test
+  void testUpdateScore() {
+  }
+
+  @Test
+  void testFinishGame() {
+  }
+}
