62f82fce5e86e10e53b2832e156551b953cd0a96
Jarosław Kątnik <jarek@katnik.pl>
2023-12-13 19:54:43 +0100

    comparator

--- src/main/java/com/sportradar/scoreboard/ByScoreAndTimeMatchComparator.java
+++ src/main/java/com/sportradar/scoreboard/ByScoreAndTimeMatchComparator.java
@@ -1,11 +1,22 @@
 package com.sportradar.scoreboard;
 
 /**
- * Comparator that sorts matches by total score (home + away) and then by start time (most recent)
+ * Comparator that sorts matches by total score (home + away) and then by start time (most recent first).
  */
 final class ByScoreAndTimeMatchComparator implements MatchComparator {
   @Override
   public int compare(Game m1, Game m2) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+    int scoreCompare = compareScore(m1, m2);
+
+    if (scoreCompare != 0) {
+      return scoreCompare;
+    }
+    return Long.compare(m1.startTime(), m2.startTime());
+  }
+
+  private int compareScore(Game m1, Game m2) {
+    int totalScore1 = m1.homeScore() + m1.awayScore();
+    int totalScore2 = m2.homeScore() + m2.awayScore();
+    return Integer.compare(totalScore1, totalScore2);
   }
 }
---
+++ src/test/java/com/sportradar/scoreboard/ByScoreAndTimeMatchComparatorTest.java
@@ -0,0 +1,38 @@
+package com.sportradar.scoreboard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ByScoreAndTimeMatchComparatorTest {
+  private MatchComparator comparator = new ByScoreAndTimeMatchComparator();
+
+  @Test
+  void shouldSortByScore() {
+    // given
+    Game game1 = givenGame("Home", "Away", 2, 0);
+    Game game2 = givenGame("Home2", "Away2", 0, 1);
+
+    // when
+    int result = comparator.compare(game1, game2);
+    // then
+    assertThat(result).isGreaterThan(0);
+  }
+
+  @Test
+  void shouldSortByScoreAndTime() throws InterruptedException {
+    // given
+    Game game1 = givenGame("Home", "Away", 2, 0);
+    Thread.sleep(1);
+    Game game2 = givenGame("Home2", "Away2", 2, 0); // this is more recent game
+
+    // when
+    int result = comparator.compare(game1, game2);
+    // then
+    assertThat(result).isLessThan(0);
+  }
+
+  private Game givenGame(String home, String away, int homeScore, int awayScore) {
+    return new Game(home, away).updateScore(homeScore, awayScore);
+  }
+}
